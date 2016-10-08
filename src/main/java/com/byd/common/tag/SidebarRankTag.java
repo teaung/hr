a32e9cc962bec77a37b69298966e5b42135512ab
hehg <hehaigang@byd.com>
2016-10-08 16:02:28 +0800

    1) add .gitignore 2) close some debug info from log4j 3) change SidebarRankTag class for admin and general user.
    	new file:   .gitignore
    	modified:   src/main/java/com/byd/common/tag/SidebarRankTag.java
    	modified:   src/main/resources/config/database.properties
    	modified:   src/main/resources/log4j.properties

--- src/main/java/com/byd/common/tag/SidebarRankTag.java
+++ src/main/java/com/byd/common/tag/SidebarRankTag.java
@@ -11,26 +11,31 @@ import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
-import com.byd.entity.Constants;
 import com.byd.shiro.entity.Resource;
 import com.byd.shiro.entity.Role;
 import com.byd.shiro.entity.User;
 
 public class SidebarRankTag extends SimpleTagSupport{
+	
+	public final String RESOURCE_TYPE_MENU = "menu";
+	public final String RESOURCE_TYPE_FUNC = "function";
+	
+	public final String ROLE_ADMIN = "admin";
+	
 	private User currentUser;
 	@Override
 	public void doTag() throws JspException, IOException {
 		List<Resource> menuResources=new ArrayList<Resource>();
-		List<Resource> submenuResources=new ArrayList<Resource>();
-		new ArrayList<Resource>();
+		List<Resource> functionResources=new ArrayList<Resource>();
+		
 		for(Role role:currentUser.getRoles()){
-			Set<Resource> resources=role.getResources();			
+			Set<Resource> resources=role.getResources();
 			for(Resource res : resources){
-				if(res!=null&&res.getType().equals("menu")){
+				if(res!=null&&res.getType().equals(RESOURCE_TYPE_MENU)){
 					menuResources.add(res);
 				}
-				if(res!=null&&res.getType().equals("submenu")){
-					submenuResources.add(res);
+				if(res!=null&&res.getType().equals(RESOURCE_TYPE_FUNC)){
+					functionResources.add(res);
 				}
 			}			
 		}		
@@ -40,46 +45,70 @@ public class SidebarRankTag extends SimpleTagSupport{
 				return o1.getPriority().compareTo(o2.getPriority());
 			}
 		});
-		Collections.sort(submenuResources, new Comparator<Resource>() {
+		Collections.sort(functionResources, new Comparator<Resource>() {
 			@Override
 			public int compare(Resource o1, Resource o2) {
 				return o1.getPriority().compareTo(o2.getPriority());
 			}
 		});
-		JspWriter out=getJspContext().getOut();		
-		for(Resource res : menuResources){
-			if(res.getUrl().equals("druid"))
-			{
-				out.println("<li><a  href='"+res.getUrl()+"' target='_blank'><i class='fa fa-laptop'></i><span>"+res.getName()+"</span></a></li>");
+		JspWriter out=getJspContext().getOut();
+		
+		Boolean bAdmin = false;
+		Set<Role> roles = currentUser.getRoles();
+		for (Role r : roles) {
+			if (r.getName().toString().equals(ROLE_ADMIN)) {
+				bAdmin = true;
+				break;
 			}
-			else if(res.getName().equals("用户管理")||res.getName().equals("角色管理")||res.getName().equals("资源管理"))
-			{
+		}
+		if (bAdmin) { // 管理员用户
+			for(Resource res : menuResources){
+				if(res.getUrl().equals("druid"))
+				{
+					out.println("<li><a  href='"+res.getUrl()+"' target='_blank'><i class='fa fa-laptop'></i><span>"+res.getName()+"</span></a></li>");
+				}
+				//else if(res.getName().equals("用户管理")||res.getName().equals("角色管理")||res.getName().equals("资源管理"))
+				else if (!res.getUrl().toString().isEmpty())
+				{
+					out.println("<li><a class='sidebarMenuHref' href='"+res.getUrl()+"'><i class='fa  fa-circle-o'></i><span>"+res.getName()+"</span></a>");
+					out.println("</li>");
+				}
+				else
+				{	
+					out.println(" <li class='treeview'>");
+					out.println("<a href='#'>");
+					out.println("<i class='fa  fa-circle-o'></i> <span>"+res.getName()+"</span>");
+					out.println("<span class='pull-right-container'>");
+					out.println("<i class='fa fa-angle-left pull-right'></i>");
+					out.println("</span>");
+					out.println("</a>");
+					out.println("<ul class='treeview-menu'>");
+					for(Resource subres : functionResources)
+					{
+						if(res.getId()==subres.getParentId())
+						{
+							out.println("<li><a class='sidebarMenuHref' href='"+subres.getUrl()+"'><i class='fa fa-circle-o'></i>"+subres.getName()+"</a></li>");
+						}
+	
+					}
+					out.println("</ul>");
+					out.println("</li>");
+				}
+					
+			}
+		}
+		else { // 普通用户
+			for(Resource res : menuResources) {
+				if (!res.getUrl().toString().isEmpty())
+				{
+					out.println("<li><a class='sidebarMenuHref' href='"+res.getUrl()+"'><i class='fa  fa-circle-o'></i><span>"+res.getName()+"</span></a>");
+					out.println("</li>");
+				}
+			}
+			for (Resource res: functionResources) {
 				out.println("<li><a class='sidebarMenuHref' href='"+res.getUrl()+"'><i class='fa  fa-circle-o'></i><span>"+res.getName()+"</span></a>");
 				out.println("</li>");
 			}
-			else
-			{	
-				out.println(" <li class='treeview'>");
-				out.println("<a href='#'>");
-				out.println("<i class='fa  fa-circle-o'></i> <span>"+res.getName()+"</span>");
-				out.println("<span class='pull-right-container'>");
-				out.println("<i class='fa fa-angle-left pull-right'></i>");
-				out.println("</span>");
-				out.println("</a>");
-				out.println("<ul class='treeview-menu'>");
-				for(Resource subres : submenuResources)
-				{
-					if(res.getId()==subres.getParentId())
-					{
-						out.println("<li><a class='sidebarMenuHref' href='"+subres.getUrl()+"'><i class='fa fa-circle-o'></i>"+subres.getName()+"</a></li>");
-					}
-
-				}
-				out.println("</ul>");
-				out.println("</li>");
-			}
-				
-
 		}
 	}
 	public User getCurrentUser() {
